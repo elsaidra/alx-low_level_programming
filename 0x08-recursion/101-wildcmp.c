cd19f9a349e5814ec315e247b1e406d900dd07ab
elsaid <alghnam906@gmail.com>
2023-04-06 00:06:27 +0200

    //

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,29 +1,40 @@
-include "main.h"
+int search_other(char s, char *str, int *counter)
+{
+	if (s == *str)
+		return (1);
+	else if (*str == '\0')
+		return (0);
+	else if (s == '*')
+		return (0);
+	else
+	{
+		*counter += 1;
+		return (search_other(s, ++str, &*counter));
+	}
+}
 
-/**
- * wildcmp - compares two strings and returns 1 if the strings
- * can be considered identical, otherwise return 0.
- * @s1: string to compare to
- * @s2: string with wild character
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int wildcmp(char *s1, char *s2)
 {
+	int counter;
+
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	if (*s2 == '*')
+	else if (*s1 == *s2)
+		return (wildcmp(++s1, ++s2));
+	else if (*s1 == '*')
 	{
-		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-			return (0);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
+		counter = 0;
+		search_other(*++s1, s2, &counter);
+		s2 += counter;
+		return (wildcmp(s1++, s2++));
 	}
-
-	return (0);
+	else if (*s2 == '*')
+	{
+		counter = 0;
+		search_other(*++s2, s1, &counter);
+		s1 += counter;
+		return (wildcmp(s1++, s2++));
+	}
+	else
+		return (0);
 }
